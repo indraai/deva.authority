b215a007c9119f80c29f846dbc2ee55bcab05345
Quinn Michaels <quinn@indra.ai>
2025-09-23 18:28:24 -0700

    Update Authority Deva to VLA:21484631554539747920

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 
 // Authority Deva
 import Deva from '@indra.ai/deva';
+import { MongoClient, ObjectId } from 'mongodb';
+
 import pkg from './package.json' with {type:'json'};
 const {agent,vars} = pkg.data;
 
@@ -38,27 +40,72 @@ const AUTHORITY = new Deva({
     process(input) {return input.trim();}
   },
   listeners: {
-    'devacore:question'(packet) {
-      const echo = this.methods.echo('authority', 'q', packet);
+    // log the start packet when the Deva enters after start
+    'devacore:start'(packet) {
+      this.func.write_log('start', packet);
     },
-    'devacore:answer'(packet) {
-      const echo = this.methods.echo('authority', 'a', packet);
-    }
+    // log the enter packet when the Deva enters after start
+    'devacore:enter'(packet) {
+      this.func.write_log('enter', packet);
+    },
+    // log the stop packet when the deva exits the system
+    'devacore:stop'(packet) {
+      this.func.write_log('stop', packet);
+    },
+    // log the exit packet when the deva exits the system
+    'devacore:exit'(packet) {
+      this.func.write_log('exit', packet);
+    },
+    // log the answer on complete of the question and answer
+    'devacore:complete'(packet) {
+      this.func.write_log('complete', packet);
+    },
   },
   modules: {},
-  deva: {},
-  func: {},
+  func: {
+    /**************
+    func: write_log
+    params: type, packet
+    describe: this is the log file writer function that handles 
+    writing for finish and complete for complete chain of custody.
+    ***************/
+    async write_log(type, packet) {
+      let result = false;
+      try {
+        const database = this.modules.client.db(this.vars.log);
+        const collection = database.collection(type);
+        result = await collection.insertOne(packet);
+      } catch (e) {
+        return this.err(e, packet, false);
+      } finally {
+        return result;
+      }
+    },    
+  },
   methods: {},
   onInit(data, resolve) {
     const {personal} = this.license(); // get the license config
     const agent_license = this.info().VLA; // get agent license
     const license_check = this.license_check(personal, agent_license); // check license
     // return this.start if license_check passes otherwise stop.
+    this.action('return', `onInit:${data.id.uid}`);    
     return license_check ? this.start(data, resolve) : this.stop(data, resolve);
   }, 
   onReady(data, resolve) {
     const {VLA} = this.info();
+    
+    this.state('get', `mongo:global:${data.id.uid}`);
+    const {uri,database, log} = this.security().global.mongo;
+    this.state('set', `mongo:client:${data.id.uid}`);
+    this.modules.client = new MongoClient(uri);
+    this.state('set', `mongo:database:${data.id.uid}`);
+    this.vars.database = database;
+    this.state('set', `mongo:log:${data.id.uid}`);
+    this.vars.log = log;
+
+    
     this.prompt(`${this.vars.messages.ready} > VLA:${VLA.uid}`);
+    this.action('resolve', `onReady:${data.id.uid}`);
     return resolve(data);
   },
   onError(err, data, reject) {
